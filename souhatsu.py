598ec8d062c36418afdfcffcd8c98c46821cfe81
坂田亘 <sakatawataru@afaa.local>
2016-05-05 13:19:40 +0900

    add

--- souhatsu.py
+++ souhatsu.py
@@ -2,16 +2,17 @@ import random
 from enum import Enum
 
 class Hai(Enum):
-    SU1 = (1, '一索')
-    SU2 = (2, '一索')
-    SU3 = (3, '一索')
-    SU4 = (4, '一索')
-    SU5 = (5, '一索')
-    SU6 = (6, '一索')
-    SU7 = (7, '一索')
-    SU8 = (8, '一索')
-    SU9 = (9, '一索')
-    HATSU = (None, '發')
+    SU1 = (1, '一索', 'yi')
+    SU2 = (2, '二索', 'er')
+    SU3 = (3, '三索', 'san')
+    SU4 = (4, '四索', 'si')
+    SU5 = (5, '五索', 'wu')
+    SU6 = (6, '六索', 'liu')
+    SU7 = (7, '七索', 'qi')
+    SU8 = (8, '八索', 'ba')
+    SU9 = (9, '九索', 'jiu')
+    HATSU = (0, '發', 'fa')
+    DS5 = (5, '赤五', 'rw')
 
     @classmethod
     def valueAt(cls, num):
@@ -19,6 +20,12 @@ class Hai(Enum):
             if hai.number == num:
                 return hai
         return None
+
+    @classmethod
+    def AllHai(cls):
+        allhai = [hai for hai in cls if not hai.number == 5] * 4
+        allhai += [cls.SU5] * 3 + [cls.DS5]
+        return allhai
 
     @property
     def number(self):
@@ -29,20 +36,31 @@ class Hai(Enum):
         return self.value[1]
 
     @property
+    def chiname(self):
+        return self.value[2]
+
+    @property
     def is_suhai(self):
         return self.number != None
 
+    @property
+    def is_ryuhai(self):
+        return self.number in [0,2,3,4,6,8]
+
 
 class Hand():
-    def __init__(self, deck):
+    def __init__(self, deck, initnum = 8):
+        self.hand = list()
         self.contents = [0]*10
-        for i in range(6):
-            a = deck.draw()
-            self.contents[a] += 1
+        for i in range(initnum):
+            tsumohai = deck.draw()
+            self.contents[tsumohai.number] += 1
+            self.hand.append(tsumohai)
             # self.contents[deck.draw()] += 1
         self.head = int()
         self.mentsu = []
         self.tsumohai = -1
+        self.priority = None
 
     def __len__(self):
         return self.contents.__len__()
@@ -66,13 +84,13 @@ class Hand():
         # print(hand)
         if hand == [0]*10:
             return True
-        if hand[9] >= 3:
-            hand[9] -= 3
-            self.mentsu.append(9)
+        if hand[0] >= 3:
+            hand[0] -= 3
+            self.mentsu.append(0)
             if self.mentsu_check(hand,count+1):
                     return True
 
-        for i in range(7):
+        for i in range(1,8):
             if hand[i] >= 3:
                 hand[i] -= 3
                 self.mentsu.append(i)
@@ -89,6 +107,22 @@ class Hand():
             self.mentsu = []
             return False
 
+    def hora_process(self):
+        contents = list(self.contents)
+        hand = list(self.hand)
+        def reach(contents):
+            return self.reachflag
+
+        def tannyao(contents):
+            return contents[0] == 0 \
+                    and contents[1] == 0 \
+                    and contents[9] == 0
+
+        def tsumo(contents):
+            return self.ronhai == None
+
+
+
     def hora_flag(self):
         for i, number in enumerate(self.contents):
             hand = list(self)
@@ -102,30 +136,45 @@ class Hand():
             return False
 
     def show_hand(self):
-        hand = list(self)
+        hand = list(self.contents)
         if self.tsumohai == -1:
             for i in range(10):
                 for j in range(hand[i]):
-                    print(repr(i+1) + ",", end="")
+                    print(repr(i) + ",", end="")
+            print() 
+            for item in self.hand:
+                print(item.hainame, end=',')
+            print()
+
             print()
             return 0
-        hand[self.tsumohai] -= 1
+
+        hand[self.tsumohai.number] -= 1
         for i in range(10):
             for j in range(hand[i]):
-                print(repr(i+1) + ",", end="")
-        print("T" + repr(self.tsumohai+1))
+                print(repr(i) + ",", end="")
+                pass
+        print("T" + str(self.tsumohai.number))
+        self.hand.remove(self.tsumohai)
+        for item in self.hand:
+            print(item.hainame, end=',')
+        print("T" + self.tsumohai.hainame)
+        self.hand.append(self.tsumohai)
 
     def trash(self, n):
-        self[n-1] -= 1
+        self[n] -= 1
+        self.hand.remove(Hai.valueAt(n))
 
     def tsumo(self, deck):
         self.tsumohai = deck.draw()
-        self[self.tsumohai] += 1
+        self.hand.append(self.tsumohai)
+        self.contents[self.tsumohai.number] += 1
 
 
 class Deck():
     def __init__(self):
-        self.deck = list(range(10))*4
+        self.deck = Hai.AllHai()
+        self.shuffle()
 
     def shuffle(self):
         random.shuffle(self.deck)
@@ -134,31 +183,82 @@ class Deck():
         return self.deck.pop()
 
 
+class Field():
+
+    def __init__(self):
+        self.deck = Deck()
+        self.myhand = Hand(self.deck)
+        self.yourhand = Hand(self.deck, initnum=7)
+        self.myriver = []
+        self.yourriver = []
+
+        self.turn = 1
+        while self.oneturn():
+            pass
+
+
+    def oneturn(self): 
+        if self.turn != 1 and hand.priority == True:
+            self.myhand.tsumo(self.deck)
+        self.show_field()
+        if self.myhand.hora_flag():
+            if self.turn == 1:
+                self.myhand.tenho_flag = True
+                #後で書く
+            self.myhand.tsumo_flag = True
+            #後で書く
+            print("You win")
+            return False
+        command = int(input())
+        self.myriver.append(Hai.valueAt(command))
+        self.myhand.trash(command)
+
+        self.yourhand.tsumo(self.deck)
+        if self.yourhand.hora_flag():
+            if self.turn == 1:
+                self.yourhand.chiho_flag = True
+            print("\n\nYou lose\n\n")
+            self.yourhand.show_hand()
+            return False
+
+#        self.naki = self.nakicheck(self.yourhand.hand[0])
+#        if self.naki != []:
+#            print self.naki
+
+        self.yourriver.append(self.yourhand.hand[0])
+        self.yourhand.trash(self.yourhand.hand[0].number)
+        return True
+
+    def show_field(self):
+        print()
+        print("turn:" + str(self.turn))
+        for hai in self.yourriver:
+            print(hai.hainame, end="")
+        print()
+        for hai in self.myriver:
+            print(hai.hainame, end="")
+        print()
+
+        print()
+        self.myhand.show_hand()
+
+
 def gameinit(myhand, deck):
     deck.shuffle()
 
+Field()
 
-def myturn(myhand, deck):
-
-    myhand.tsumo(deck)
-    myhand.show_hand()
-    if myhand.hora_flag():
-        print("goal")
-        return False
-    myhand.trash(int(input()))
-    return True
 """
 deck = Deck()
 deck.shuffle()
 myhand = Hand(deck)
+ophand = Hand(deck)
 myhand.show_hand()
 if myhand.hora_flag():
     print("goal")
 myhand.trash(int(input()))
 while myturn(myhand, deck):
     pass
-"""
-
 deck = Deck()
 deck.shuffle()
 myhand = Hand(deck)
@@ -167,3 +267,4 @@ myhand.tsumohai = 0
 print(myhand.hora_flag())
 print(myhand.mentsu[0])
 myhand.show_hand()
+"""
